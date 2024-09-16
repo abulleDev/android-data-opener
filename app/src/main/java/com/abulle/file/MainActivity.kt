1aef72b5848e22946f063bb6f6fe6388b92afdce
abulle <olives2739@gmail.com>
2024-09-16 15:57:42 +0900

    feat: Add file move feature

--- app/src/main/java/com/abulle/file/MainActivity.kt
+++ app/src/main/java/com/abulle/file/MainActivity.kt
@@ -17,7 +17,7 @@ class MainActivity : ComponentActivity() {
         val folderUri = Uri.parse("content://com.android.externalstorage.documents/document/primary%3AAndroid%2Fdata%2F");
 
         // Create intent
-        val intent = Intent(Intent.ACTION_VIEW)
+        val intent1 = Intent(Intent.ACTION_VIEW)
             .setPackage("com.google.android.documentsui")
             .setDataAndType(folderUri, "vnd.android.document/directory")
             .addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
@@ -28,12 +28,26 @@ class MainActivity : ComponentActivity() {
                 "com.android.documentsui.files.FilesActivity"
             );
 
+        val intent2 = Intent(Intent.ACTION_VIEW)
+            .setPackage("com.google.android.documentsui")
+            .setDataAndType(folderUri, "vnd.android.document/directory")
+            .addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
+            .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            .addFlags(Intent.FLAG_RECEIVER_FOREGROUND)
+            .addFlags(Intent.FLAG_ACTIVITY_MULTIPLE_TASK)
+            .addFlags(Intent.FLAG_ACTIVITY_LAUNCH_ADJACENT)
+            .setClassName(
+                "com.google.android.documentsui",
+                "com.android.documentsui.files.FilesActivity"
+            );
+
         // Remove app from recent history
         val activityManager = getSystemService(ACTIVITY_SERVICE) as ActivityManager;
         activityManager.appTasks[0].setExcludeFromRecents(true);
 
         try {
-            startActivity(intent);
+            // Run in split screen mode
+            startActivities(arrayOf(intent1, intent2))
             Toast.makeText(this@MainActivity, "Open Android data directory.", Toast.LENGTH_SHORT).show();
             finish();
         } catch (e: ActivityNotFoundException) {
